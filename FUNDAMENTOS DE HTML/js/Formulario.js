5609f25ded30347be6f8860d8418a3a4f84b6020
Deyner <30375815+Deyfunes@users.noreply.github.com>
2017-10-27 18:12:16 -0600

    Add files via upload

---
+++ FUNDAMENTOS DE HTML/js/Formulario.js
@@ -0,0 +1,13 @@
+
+//
+
+var List = document.getElementById("List").value;
+
+var Institución = document.getElementById("Insitución").value;
+
+var Lugar = document.getElementById("Lugar").value;
+
+
+
+
+
---
+++ FUNDAMENTOS DE HTML/js/addstudio.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+ 
+
+var Año = document.getElementById("list").value;
+
+var Institución = document.getElementById("Institución").value;
+
+var Lugar = document.getElementById("Lugar").value;
+
+ 
+
+//función mostrar alerta
+
+function mostrarAlerta(mensaje){
+	
+	alert('Sus datos se enviaron correctamente');
+	
+}
+	
+	boton.addEventListener('click', mostrarAlerta);
+	
+	
+	
+	
